9010e8de8d74b7ed842ed0238f8b00603842a921
Gautammer <mergautam9@gmail.com>
2022-08-10 14:50:53 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const stripe = require("stripe")(STRIPE_SECRET_KEY);
 app.use(express.json());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100"); // for ionic serve
+  //  res.header("Access-Control-Allow-Origin", "http://localhost:8100"); // for ionic serve
      res.header("Access-Control-Allow-Origin", "capacitor://localhost"); // for ionic capacitor run ios/android
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
